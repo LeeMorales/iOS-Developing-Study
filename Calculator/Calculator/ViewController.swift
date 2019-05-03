d8195fd7c73d6c3180e7ccb2a8f9fbd07ec6e4ea
Lee Morales <grandtheftauto710@outlook.com>
2019-05-03 15:43:54 +0800

    文件删了重新来

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,62 +12,15 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var Display: UILabel!
     var userIsInTheMiddleOfTypingANumber:Bool = false
-    @IBAction func appendDigit(_ sender: UIButton) {
+    
+    @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInTheMiddleOfTypingANumber{
-        Display.text = Display.text! + digit
+            Display.text = Display.text! + digit
         }else{
             Display.text = digit
             userIsInTheMiddleOfTypingANumber = true
         }
-    
-}
-    @IBAction func operate(sender: UIButton) {
-        let operation = sender.currentTitle
-        if userIsInTheMiddleOfTypingANumber {
-            enter()
-        }
-        switch operation {
-        case "✖️":performOperation(operation: multiply)
-        case "➗":performOperation(operation: divide)
-        case "➕":performOperation(operation: plus)
-        case "➖":performOperation(operation: minus)
-        default: break
-        }
-    }
-    func performOperation(operation:(Double, Double) -> Double){
-        if operandStack.count >= 2 {
-            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
-            enter()
-        }
-    }
-    func multiply(op1: Double, op2: Double) -> Double{
-        return op1 * op2
-    }
-    func divide(op1: Double, op2: Double) -> Double{
-        return op1 / op2
-    }
-    func plus(op1: Double, op2: Double) -> Double{
-        return op1 + op2
-    }
-    func minus(op1: Double, op2: Double) -> Double{
-        return op1 - op2
     }
     
-    var operandStack = Array<Double>()
-    
-    @IBAction func enter() {
-        userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
-        print("operandStack = \(operandStack)")
-    }
-    var displayValue: Double{
-        get{
-            return NumberFormatter().number(from: Display.text!)!.doubleValue
-        }
-        set{
-            Display.text = "\(newValue)"
-            userIsInTheMiddleOfTypingANumber = false
-        }
-    }
 }
