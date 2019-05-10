7bc739d5ba0de5cc42f202a207275b21ab712d96
Lee Morales <grandtheftauto710@outlook.com>
2019-05-10 21:35:47 +0800

    加入反转支持

--- Concentraction/Concentraction/Concentraction.swift
+++ Concentraction/Concentraction/Concentraction.swift
@@ -12,7 +12,26 @@ class Concentraction {
     
     var cards = [Card]()
     
-    var indexOfOneAndOnlyFaceUpCard: Int?
+    var indexOfOneAndOnlyFaceUpCard: Int?{
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    }else{
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
         if !cards[index].isMatched {
@@ -22,13 +41,8 @@ class Concentraction {
                     cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnlyFaceUpCard = nil
             }else{
                 //either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                cards[index].isFaceUp = true
                 indexOfOneAndOnlyFaceUpCard = index
             }
         }
--- Concentraction/Concentraction/ViewController.swift
+++ Concentraction/Concentraction/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    lazy var game: Concentraction = Concentraction(numberOfPairsOfCards: (cardButtons.count + 1) / 2)
+    lazy var game: Concentraction = Concentraction(numberOfPairsOfCards: numberOfPairsOfCards)
+    
+    var numberOfPairsOfCards: Int {
+            return ((cardButtons.count+1) / 2)
+    }
     
     var flipCount = 0 { didSet { flipCountLabel.text = "Flips: \(flipCount)" } }
     
@@ -39,7 +43,7 @@ class ViewController: UIViewController {
                 button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
             }else{
                 button.setTitle("", for:UIControl.State.normal)
-                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9506973624, green: 0.9854659438, blue: 0.9847254157, alpha: 0) : #colorLiteral(red: 1, green: 0.6216369271, blue: 0.4910389185, alpha: 1)
+                button.backgroundColor = card.isMatched ? #colorLiteral(red: 0.9506973624, green: 0.9854659438, blue: 0.9847254157, alpha: 0) : #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
             }
         }
     }
