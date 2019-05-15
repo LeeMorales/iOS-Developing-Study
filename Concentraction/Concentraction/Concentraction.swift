d16c8031b5beb1f70a9843950a3a4312fdf7a8b9
Lee Morales <grandtheftauto710@outlook.com>
2019-05-15 21:37:19 +0800

    加入应用集的稳定

--- Concentraction/Concentraction/Concentraction.swift
+++ Concentraction/Concentraction/Concentraction.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Concentraction {
+struct Concentraction {
     
     private(set) var cards = [Card]()
     
@@ -33,7 +33,7 @@ class Concentraction {
         }
     }
     
-    func chooseCard(at index: Int) {
+    mutating func chooseCard(at index: Int) {
         assert(cards.indices.contains(index), "Concentraction,chooseCard(at: \(index)): chosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index {
