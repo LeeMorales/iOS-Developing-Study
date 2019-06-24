4e77479493a65cb7d04dbfada85e47c8eaaf5417
Lee Morales <grandtheftauto710@outlook.com>
2019-06-24 17:46:20 +0800

    加入全新的应用

---
+++ Checklists/Checklists/CheckListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Lee Morales on 2019/6/24.
+//  Copyright © 2019 Lee Morales. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
